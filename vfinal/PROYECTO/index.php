4cb9ac1afacabc96130d9a10d57e8aec88f43ee1
German Zambrana Ruiz <32089005+GermanZR98@users.noreply.github.com>
2018-11-28 13:08:39 +0100

    Update index.php

--- vfinal/PROYECTO/index.php
+++ vfinal/PROYECTO/index.php
@@ -71,16 +71,6 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
 </head>
-<header>
-<?php
-if (isset($_GET["exito"])):
-    echo "<h3 style=\"color: green\">El registro se ha realizado correctamente</h3>" ;
-   endif ;
-
-
-   
-?>
-</header>
 <body>
 	<div class="formOuter">
 		<div class="container">
